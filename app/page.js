a922e47672ce7b1084d764efed5ecc87a3d56da6
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-05 12:42:40 +0600

    homepage

--- app/page.js
+++ app/page.js
@@ -1,3 +1,9 @@
+import HomeSection from '@/components/Home/HomeSection';
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <HomeSection />
+    </>
+  );
 }
---
+++ components/Home/HomeSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeSection = () => {
+  return (
+    <div>
+      <h1>Home section</h1>
+    </div>
+  );
+};
+
+export default HomeSection;
