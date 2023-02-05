312e31cccbec3bacfe6967da06e76af72ea59ced
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-05 14:12:58 +0600

    navbar changes

--- components/Shared/Navbar/Navbar.jsx
+++ components/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,61 @@
+'use client';
+
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+  const menuItems = (
+    <>
+      <li>
+        <Link href={'/'}>Home</Link>
+      </li>
+      <li>
+        <Link href={'/about_me'}>About</Link>
+      </li>
+      <li>
+        <Link href={'/blogs'}>Blogs</Link>
+      </li>
+    </>
+  );
   return (
-    <div>
-      <h1>Hello navbar</h1>
+    <div className="navbar">
+      <div className="navbar-start">
+        <button className="btn btn-ghost normal-case text-xs sm:text-sm md:text-xl text-primary">
+          <Link href={'/'} className="">
+            {/* <FontAwesomeIcon icon={faCode} className="mr-2" size="lg" />{' '} */}
+            <span className="text-white">Muhammad Azizul Hoque Jiku</span>
+          </Link>
+        </button>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 glass"
+          >
+            {menuItems}
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+      </div>
     </div>
   );
 };
