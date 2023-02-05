992d70abe8d6339bf4f1250c561ba646dca6550f
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-05 13:39:21 +0600

    _app.js added

--- components/Shared/NotFound/NotFound.jsx
+++ components/Shared/NotFound/NotFound.jsx
@@ -1,9 +1,23 @@
+'use client';
+
 import React from 'react';
+import Link from 'next/link';
+import NotFoundImg from '../../../assets/gifs/notfound1.gif';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1> 404 </h1>
+    <div className="hero min-h-full">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <img src={NotFoundImg.src} alt="not found" />
+          <h1 className="text-3xl font-bold py-4">Page is not found!</h1>
+          <button className="btn btn-primary text-white font-bold cursor-pointer">
+            <Link href={`/`} className="">
+              Go to Home
+            </Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
