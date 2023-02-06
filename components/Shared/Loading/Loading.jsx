210aa221302c928a6c132d0d7682710d50e5fd1e
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-06 13:08:18 +0600

    some definite changes

--- components/Shared/Loading/Loading.jsx
+++ components/Shared/Loading/Loading.jsx
@@ -5,7 +5,7 @@ import loadingImg from '../../../assets/gifs/loading.gif';
 
 const LoadingSpinner = () => {
   return (
-    <div className="hero min-h-screen">
+    <div className="hero min-h-full">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <img src={loadingImg.src} alt="loading" />
