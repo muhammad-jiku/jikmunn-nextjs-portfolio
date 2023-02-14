7fdb4b377e4b04ea58877d711ef829d850e061e5
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-14 23:48:26 +0600

    recent projects files animated

--- components/Home/RecentProjects/RecentProjects.jsx
+++ components/Home/RecentProjects/RecentProjects.jsx
@@ -1,10 +1,22 @@
+'use client';
+
 import React from 'react';
+import { motion } from 'framer-motion';
+import { staggerContainer } from '@/utils/motion';
+import TypingText from '@/components/Shared/CustomTexts/CustomTexts';
 
 const RecentProjects = () => {
   return (
-    <div>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="container mx-auto p-3 my-6"
+    >
+      <TypingText title="Recent Projects" />
       <h1>RecentProjects</h1>
-    </div>
+    </motion.div>
   );
 };
 
