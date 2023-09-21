822f2fe171942446b856f6cd1034bd8abf76df61
muhammad-jiku <muhammadjiku364@gmail.com>
2023-09-21 21:43:28 +0600

    projects data added

---
+++ db/index.js
@@ -0,0 +1,41 @@
+export const projectsData = [
+  {
+    id: 1,
+    projectTitle: 'Sports Mania',
+    projectLink: 'https://jikmunn-sportsmania.vercel.app/',
+    projectCodeLink: 'https://github.com/muhammad-jiku/jikmunn-sportsmania',
+    photo:
+      'https://res.cloudinary.com/drlcxykvf/image/upload/v1694959537/_my_portfolio_/images/-sports-mania_knyfpq.png',
+    video:
+      'https://res.cloudinary.com/drlcxykvf/video/upload/v1695143090/_my_portfolio_/videos/complete_sportsmania_demo_2.0_uthw9d.mp4',
+    langs:
+      'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    desc: 'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+  },
+  {
+    id: 2,
+    projectTitle: `Dr. Abdul Kader's Personal Appointment Website`,
+    projectLink: 'https://jikmunn-dr-abdul-kader.vercel.app/',
+    projectCodeLink: 'https://github.com/muhammad-jiku/jikmunn-dr-abdul-kader',
+    photo:
+      'https://res.cloudinary.com/drlcxykvf/image/upload/v1694959536/_my_portfolio_/images/-dr-abdul-kader_gserlm.png',
+    video:
+      'https://res.cloudinary.com/drlcxykvf/video/upload/v1695143105/_my_portfolio_/videos/complete_doctor_pintu_demo_f1a2j0.mp4',
+    langs:
+      'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    desc: 'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+  },
+  {
+    id: 3,
+    projectTitle: 'Conference Call App',
+    projectLink: 'https://jikmunn-conference-app.vercel.app/',
+    projectCodeLink: 'https://github.com/muhammad-jiku/jikmunn-conference-app',
+    photo:
+      'https://res.cloudinary.com/drlcxykvf/image/upload/v1694959536/_my_portfolio_/images/-video-sdk_c4cor5.jpg',
+    video:
+      'https://res.cloudinary.com/drlcxykvf/video/upload/v1695143099/_my_portfolio_/videos/complete_conference_call_app_demo_djohzf.mp4',
+    langs:
+      'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    desc: 'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+  },
+];
